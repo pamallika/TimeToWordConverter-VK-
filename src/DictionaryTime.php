cd41cbb7910aa1944023d5acf5486a08af402a2c
pamallika <sasha.fedorenko4736@gmail.com>
2023-03-28 00:43:29 +0300

    Switch construct var to const. I didn't understand why I decided to initially use a constructor, and not constants)))

--- src/DictionaryTime.php
+++ src/DictionaryTime.php
@@ -4,125 +4,97 @@ namespace src;
 
 class DictionaryTime implements DictionaryTimeInterface
 {
-    /**
-     * Словарь(мапа) для работы со временем
-     **/
-    /**
-     * @var array|string[]
-     */
-    private $dictionaryMinutes;
-    /**
-     * @var array|string[]
-     */
-    private $dictionaryBeforeHours;
-    /**
-     * @var array|string[]
-     */
-    private $dictionaryAfterHours;
-    /**
-     * @var array|string[]
-     */
-    private $dictionaryHalfHours;
-    /**
-     * @var array|string[]
-     */
-    private $dictionaryHours;
-
-    public function __construct()
-    {
-        $this->dictionaryMinutes = [
-            0 => '',
-            1 => 'одна',
-            2 => 'две',
-            3 => 'три',
-            4 => 'четыре',
-            5 => 'пять',
-            6 => 'шесть',
-            7 => 'семь',
-            8 => 'восемь',
-            9 => 'девять',
-            10 => 'десять',
-            11 => 'одиннадцать',
-            12 => 'двенадцать',
-            13 => 'тринадцать',
-            14 => 'четырнадцать',
-            15 => 'пятнадцать',
-            16 => 'шестнадцать',
-            17 => 'семьнадцать',
-            18 => 'восемнадцать',
-            19 => 'девятнадцать',
-            20 => 'двадцать',
-            30 => 'половина',
-            40 => 'сорок',
-            50 => 'пятьдесят',
-            60 => '',
-        ];
-        $this->dictionaryBeforeHours = [
-            0 => 'двенадцати',
-            1 => 'часа',
-            2 => 'двух',
-            3 => 'трёх',
-            4 => 'четырёх',
-            5 => 'пяти',
-            6 => 'шести',
-            7 => 'семи',
-            8 => 'восьми',
-            9 => 'девяти',
-            10 => 'десяти',
-            11 => 'одиннадцати',
-            12 => 'двенадцати',
-            13 => 'часа',
-        ];
-        $this->dictionaryAfterHours = [
-            0 => 'двенадцати',
-            1 => 'часа',
-            2 => 'двух',
-            3 => 'трёх',
-            4 => 'четырёх',
-            5 => 'пяти',
-            6 => 'шести',
-            7 => 'семи',
-            8 => 'восьми',
-            9 => 'девяти',
-            10 => 'десяти',
-            11 => 'одиннадцати',
-            12 => 'двенадцати',
-            13 => 'часа',
-        ];
-        $this->dictionaryHalfHours = [
-            0 => 'двенадцатого',
-            1 => 'первого',
-            2 => 'второго',
-            3 => 'третьего',
-            4 => 'четвёртого',
-            5 => 'пятого',
-            6 => 'шестого',
-            7 => 'седьмого',
-            8 => 'восьмого',
-            9 => 'девятого',
-            10 => 'десятого',
-            11 => 'одиннадцатого',
-            12 => 'двенадцатого',
-            13 => 'первого',
-        ];
-        $this->dictionaryHours = [
-            0 => 'двенадцать',
-            1 => 'один',
-            2 => 'два',
-            3 => 'три',
-            4 => 'четыре',
-            5 => 'пять',
-            6 => 'шесть',
-            7 => 'семь',
-            8 => 'восемь',
-            9 => 'девять',
-            10 => 'десять',
-            11 => 'одиннадцать',
-            12 => 'двенадцать',
-            13 => 'один',
-        ];
-
-    }
+    const dictionaryMinutes = [
+        0 => '',
+        1 => 'одна',
+        2 => 'две',
+        3 => 'три',
+        4 => 'четыре',
+        5 => 'пять',
+        6 => 'шесть',
+        7 => 'семь',
+        8 => 'восемь',
+        9 => 'девять',
+        10 => 'десять',
+        11 => 'одиннадцать',
+        12 => 'двенадцать',
+        13 => 'тринадцать',
+        14 => 'четырнадцать',
+        15 => 'пятнадцать',
+        16 => 'шестнадцать',
+        17 => 'семьнадцать',
+        18 => 'восемнадцать',
+        19 => 'девятнадцать',
+        20 => 'двадцать',
+        30 => 'половина',
+        40 => 'сорок',
+        50 => 'пятьдесят',
+        60 => '',
+    ];
+    const dictionaryBeforeHours = [
+        0 => 'двенадцати',
+        1 => 'часа',
+        2 => 'двух',
+        3 => 'трёх',
+        4 => 'четырёх',
+        5 => 'пяти',
+        6 => 'шести',
+        7 => 'семи',
+        8 => 'восьми',
+        9 => 'девяти',
+        10 => 'десяти',
+        11 => 'одиннадцати',
+        12 => 'двенадцати',
+        13 => 'часа',
+    ];
+    const dictionaryAfterHours = [
+        0 => 'двенадцати',
+        1 => 'часа',
+        2 => 'двух',
+        3 => 'трёх',
+        4 => 'четырёх',
+        5 => 'пяти',
+        6 => 'шести',
+        7 => 'семи',
+        8 => 'восьми',
+        9 => 'девяти',
+        10 => 'десяти',
+        11 => 'одиннадцати',
+        12 => 'двенадцати',
+        13 => 'часа',
+    ];
+    const dictionaryHalfHours = [
+        0 => 'двенадцатого',
+        1 => 'первого',
+        2 => 'второго',
+        3 => 'третьего',
+        4 => 'четвёртого',
+        5 => 'пятого',
+        6 => 'шестого',
+        7 => 'седьмого',
+        8 => 'восьмого',
+        9 => 'девятого',
+        10 => 'десятого',
+        11 => 'одиннадцатого',
+        12 => 'двенадцатого',
+        13 => 'первого',
+    ];
+    const dictionaryHours = [
+        0 => 'двенадцать',
+        1 => 'один',
+        2 => 'два',
+        3 => 'три',
+        4 => 'четыре',
+        5 => 'пять',
+        6 => 'шесть',
+        7 => 'семь',
+        8 => 'восемь',
+        9 => 'девять',
+        10 => 'десять',
+        11 => 'одиннадцать',
+        12 => 'двенадцать',
+        13 => 'один',
+    ];
 
     /**
      * @param int $hour
@@ -130,7 +102,7 @@ class DictionaryTime implements DictionaryTimeInterface
      */
     public function getDictionaryAfterHours(int $hour): string
     {
-        return $this->dictionaryAfterHours[$hour];
+        return self::dictionaryAfterHours[$hour];
     }
 
     /**
@@ -139,7 +111,7 @@ class DictionaryTime implements DictionaryTimeInterface
      */
     public function getDictionaryBeforeHours(int $hour): string
     {
-        return $this->dictionaryBeforeHours[$hour];
+        return self::dictionaryBeforeHours[$hour];
     }
 
     /**
@@ -148,7 +120,7 @@ class DictionaryTime implements DictionaryTimeInterface
      */
     public function getDictionaryHalfHours(int $hour): string
     {
-        return $this->dictionaryHalfHours[$hour];
+        return self::dictionaryHalfHours[$hour];
     }
 
     /**
@@ -157,7 +129,7 @@ class DictionaryTime implements DictionaryTimeInterface
      */
     public function dictionaryMinutes(int $minute): string
     {
-        return $this->dictionaryMinutes[$minute];
+        return self::dictionaryMinutes[$minute];
     }
 
     /**
@@ -180,6 +152,6 @@ class DictionaryTime implements DictionaryTimeInterface
      */
     public function getDictionaryTotalHours(int $hour): string
     {
-        return  $this->dictionaryHours[$hour];
+        return  self::dictionaryHours[$hour];
     }
 }
